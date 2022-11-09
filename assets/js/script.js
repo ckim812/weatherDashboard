a7287b2f3d9fdc7c8facd592a7b632e35eeee20b
ckim812 <109961170+ckim812@users.noreply.github.com>
2022-11-08 18:24:51 -0600

    still working on local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,10 @@ let apiKey = "f09736215f8792d83b9ea278642c25cc";
 
 let savedCities = [];
 
-const init = async () => {
-  city = document.querySelector("input").value; //define input into a variable
-  if (!city) return; //exit function if no value is inputted
+let a = [];
 
-  // save cities into localStorage
-  savedCities.push(city);
-  console.log(savedCities);
-  localStorage.setItem("savedCities", savedCities);
+let displayWeather = async (city) => {
+  console.log(city);
 
   //current weather and forecast weather APIs
   let currentURL = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=${city}`;
@@ -21,61 +17,70 @@ const init = async () => {
 
   y = list;
 
-  console.log(x);
-  console.log(y);
-
   var currentTimeStamp = x.dt;
   var currentDateFormat = new Date(currentTimeStamp * 1000).toLocaleDateString(
     "en-us",
     { weekday: "long", year: "numeric", month: "short", day: "numeric" }
   );
 
-  let displayWeather = (city) => {
-    //print current weather
+  //print current weather
+  document.querySelector(
+    ".currentTitle"
+  ).innerHTML = `<h2>${city}  ${currentDateFormat}</h2>`;
+  document.querySelector(
+    ".currentIcon"
+  ).innerHTML = `<img src=http://openweathermap.org/img/wn/${x.weather[0].icon}.png></img>`;
+  document.querySelector(".currentTemp").textContent =
+    "Temperature: " + x.main.temp + " \u00B0F";
+  document.querySelector(".currentWind").textContent =
+    "Wind: " + x.wind.speed + " MPH";
+  document.querySelector(".currentHumidity").textContent =
+    "Humidity: " + x.main.humidity + " %";
+
+  //loop to print 5-day forecast
+  let z = 0;
+  for (let i = 7; i < y.length; i += 8) {
+    let date = new Date(y[i].dt * 1000).toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+    document.querySelector(`.forecast${z}Title`).innerHTML = `<h7>${date}</h7>`;
     document.querySelector(
-      ".currentTitle"
-    ).innerHTML = `<h2>${city}  ${currentDateFormat}</h2>`;
-    document.querySelector(
-      ".currentIcon"
-    ).innerHTML = `<img src=http://openweathermap.org/img/wn/${x.weather[0].icon}.png></img>`;
-    document.querySelector(".currentTemp").textContent =
-      "Temperature: " + x.main.temp + " \u00B0F";
-    document.querySelector(".currentWind").textContent =
-      "Wind: " + x.wind.speed + " MPH";
-    document.querySelector(".currentHumidity").textContent =
-      "Humidity: " + x.main.humidity + " %";
+      `.forecast${z}Icon`
+    ).innerHTML = `<img src=http://openweathermap.org/img/wn/${y[i].weather[0].icon}.png></img>`;
+    document.querySelector(`.forecast${z}Temp`).textContent =
+      "Temperature: " + y[i].main.temp + " \u00B0F";
+    document.querySelector(`.forecast${z}Wind`).textContent =
+      "Wind: " + y[i].wind.speed + " MPH";
+    document.querySelector(`.forecast${z}Humidity`).textContent =
+      "Humidity: " + y[i].main.humidity + " %";
+    z++;
+  }
+};
 
-    //loop to print 5-day forecast
-    let z = 0;
-    for (let i = 7; i < y.length; i += 8) {
-      console.log(i);
-      let date = new Date(y[i].dt * 1000).toLocaleDateString("en-us", {
-        weekday: "long",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      });
-      console.log(date);
-      document.querySelector(
-        `.forecast${z}Title`
-      ).innerHTML = `<h7>${date}</h7>`;
-      document.querySelector(
-        `.forecast${z}Icon`
-      ).innerHTML = `<img src=http://openweathermap.org/img/wn/${y[i].weather[0].icon}.png></img>`;
-      document.querySelector(`.forecast${z}Temp`).textContent =
-        "Temperature: " + y[i].main.temp + " \u00B0F";
-      document.querySelector(`.forecast${z}Wind`).textContent =
-        "Wind: " + y[i].wind.speed + " MPH";
-      document.querySelector(`.forecast${z}Humidity`).textContent =
-        "Humidity: " + y[i].main.humidity + " %";
-      console.log(`.forecast${z}Title`);
-      console.log(z);
-      z++;
-    }
-  };
+let displaySavedWeather = (city) => {
+  if (city.target.innerText) {
+    city = city.target.innerText;
+  }
+  displayWeather();
+};
 
-  displayWeather(city); //display current weather and 5-day forecast
+let init = () => {
+  let city = document.querySelector("input").value; //define input into a variable
+  if (!city) return; //exit function if no value is inputted
 
+  // save cities into localStorage
+  savedCities.push(city);
+  localStorage.setItem("savedCities", savedCities);
+
+  displayWeather(city);
+
+  displaySavedCities();
+};
+
+let displaySavedCities = () => {
   //clear all child elements from localStorage display list
   while (document.querySelector(".savedCities").firstChild) {
     document
@@ -87,20 +92,14 @@ const init = async () => {
   for (let i = 0; i < savedCities.length; i++) {
     let node = document.createElement("li"); //create list element
     let nodeBtn = node.appendChild(document.createElement("button")); //create button element under list element
-    nodeBtn.classList.add("savedCity" + i); //add class to created button
-    console.log(savedCities[i]);
+    nodeBtn.setAttribute("class", "savedCity"); //add class to created button
     nodeBtn.textContent = `${savedCities[i]}`; //add input text to button
     document.querySelector(".savedCities").appendChild(node); //display button in html
   }
-  
-};
 
-for (let i = 0; i < savedCities.length; i++) {
-  let a = document.getElementsByClassName(`${savedCities[i]}`);
-  console.log(a);
-  a      .addEventListener("click", displayWeather(savedCities[i])); //add click function to each button
-}
-//create function (or maybe use init) to display the data from localStorage
-//add event listener to each created button to run function to display data from localStorage
+  document.querySelectorAll(".savedCity").forEach((btn) => {
+    btn.addEventListener("click", displayWeather); //add click function to each button
+  });
+};
 
 document.querySelector("#searchBtn").addEventListener("click", init);
